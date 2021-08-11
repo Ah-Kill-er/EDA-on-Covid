1b07235e27b2eae961b9b1613201b83a77848069
Akhil Pratap Singh <88718162+Ah-Kill-er@users.noreply.github.com>
2021-08-12 04:33:03 +0530

    Update EDAonCOVID.sql

--- EDAonCOVID.sql
+++ EDAonCOVID.sql
@@ -196,13 +196,21 @@ WHERE continent IS NOT NULL
 --7
 
 CREATE VIEW vwPercentPopulationVaccinated AS
-SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS [Rolling Count Of People Vaccinated]
-FROM coviddeaths dea
-JOIN covidvaccinations vac
-ON dea.location = vac.location 
-AND dea.date = vac.date
-WHERE dea.continent IS NOT NULL
+WITH PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+AS
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.Location ORDER BY dea.location, dea.Date) AS RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL 
+--order by 2,3
+)
+SELECT *, (RollingPeopleVaccinated/Population)*100 AS PercentPeopleVaccinated
+FROM PopvsVac
 
 
 
